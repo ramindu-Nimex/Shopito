305ab452c53842e6721ec69ae74dff969813508b
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-08-27 11:51:57 +0530

    Add a middleware and a function to handle error

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,19 @@ app.use(express.json());
 
 dbConnection();
 
-
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3000");
 });
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});

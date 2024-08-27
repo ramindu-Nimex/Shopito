c5e8968ec136c2fee7a9702e73c966fb365cb4e1
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-08-27 17:25:03 +0530

    Add update user API route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import dbConnection from "./dbConfig/dbConnection.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -7,6 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 dbConnection();
 

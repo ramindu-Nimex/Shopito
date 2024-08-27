53802398f1a505251f0ce5d11af19a806c639724
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-08-27 11:00:06 +0530

    Connect to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import dbConnection from "./dbConfig/dbConnection.js";
 
 const app = express();
+dotenv.config();
+
+dbConnection();
+
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");

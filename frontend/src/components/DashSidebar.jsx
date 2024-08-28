965fdfa2ca4deb135a1cbb4865fda20c49c28930
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-08-28 09:55:46 +0530

    Added all the admin functionality

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -4,8 +4,10 @@ import { HiArrowSmRight, HiUser } from "react-icons/hi";
 import { useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
+import { useSelector } from "react-redux";
 
 export default function DashSidebar() {
+  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
   const dispatch = useDispatch();
   const [tab, setTab] = useState("");
@@ -41,7 +43,7 @@ export default function DashSidebar() {
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >

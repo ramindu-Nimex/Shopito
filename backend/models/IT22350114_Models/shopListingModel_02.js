0656fbbaf415b622695516b6867b84b25949d98e
thihansi <thihansig@gmail.com>
2024-08-31 10:11:41 +0530

    Add shop listing model

---
+++ backend/models/IT22350114_Models/shopListingModel_02.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const shopListingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ShopListing = mongoose.model("ShopListing", shopListingSchema);
+export default ShopListing;

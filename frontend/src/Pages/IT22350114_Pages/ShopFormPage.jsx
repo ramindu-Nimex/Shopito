7761f5252eb76c7a594013a9e5de2f92258198da
thihansi <thihansig@gmail.com>
2024-09-01 08:14:45 +0530

    add new fields

--- frontend/src/Pages/IT22350114_Pages/ShopFormPage.jsx
+++ frontend/src/Pages/IT22350114_Pages/ShopFormPage.jsx
@@ -6,22 +6,58 @@ import { Button, Label, TextInput, Textarea, Alert } from "flowbite-react";
 const ShopCreate = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({
-    name: "",
-    location: "",
-    description: "",
-    category: "",
+    shopID: "",
+    shopName: "",
+    shopLocation: "",
+    shopDescription: "",
+    shopCategory: "",
+    shopPhone: "",
+    shopEmail: "",
+    shopWebsite: "",
+    shopOpeningHours: "",
+    isOpen: true,
+    //imageURls: [],
   });
+  const {
+    shopID,
+    shopName,
+    shopLocation,
+    shopDescription,
+    shopCategory,
+    shopPhone,
+    shopEmail,
+    shopWebsite,
+    shopOpeningHours,
+    isOpen,
+    imageURls,
+  } = formData;
 
-  const { name, location, description, category } = formData;
-  const [error, setError] = useState("");
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    let processedValue = value;
+    if (type === "checkbox") {
+      processedValue = checked;
+    }
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: processedValue,
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      if (!name || !location || !description || !category) {
+      if (!shopName || !shopLocation || !shopDescription || !shopCategory) {
         return setError("Please fill in all fields.");
       }
 
@@ -50,11 +86,65 @@ const ShopCreate = () => {
     }
   };
 
-  const handleChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageURLs.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageURLs: formData.imageURLs.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError("Image Upload failed (2mb max per Image)");
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 Images per listing");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageURLs: formData.imageURLs.filter((_, i) => i !== index),
+    });
   };
 
   return (
@@ -65,52 +155,154 @@ const ShopCreate = () => {
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <div className="flex flex-col gap-3">
           <div>
-            <Label htmlFor="name">Shop Name</Label>
+            <Label htmlFor="shopID">Amenity ID</Label>
             <TextInput
               type="text"
-              name="name"
-              value={name}
+              name="shopID"
+              value={formData.shopID}
               onChange={handleChange}
-              id="name"
-              autocomplete="on" // Added autocomplete attribute
               required
             />
           </div>
           <div>
-            <Label htmlFor="location">Location</Label>
+            <Label htmlFor="shopName">Shop Name:</Label>
             <TextInput
               type="text"
-              name="location"
-              value={location}
+              name="shopName"
+              value={formData.shopName}
               onChange={handleChange}
-              id="location"
-              autocomplete="on" // Added autocomplete attribute
               required
             />
           </div>
           <div>
-            <Label htmlFor="description">Description</Label>
+            <Label htmlFor="shopLocation">Shop Location</Label>
+            <TextInput
+              type="text"
+              name="shopLocation"
+              value={formData.shopLocation}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="shopDescription">Shop Description:</Label>
             <Textarea
-              name="description"
-              value={description}
+              name="shopDescription"
+              value={formData.shopDescription}
               onChange={handleChange}
-              id="description"
-              autocomplete="on" // Added autocomplete attribute
               required
             />
           </div>
           <div>
-            <Label htmlFor="category">Category</Label>
+            <Label htmlFor="shopCategory">Shop Category:</Label>
             <TextInput
               type="text"
-              name="category"
-              value={category}
+              name="shopCategory"
+              value={formData.shopCategory}
               onChange={handleChange}
-              id="category"
-              autocomplete="on" // Added autocomplete attribute
               required
             />
           </div>
+          <div>
+            <Label htmlFor="shopPhone">Shop Phone:</Label>
+            <TextInput
+              type="text"
+              name="shopPhone"
+              value={formData.shopPhone}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Label htmlFor="shopEmail">Shop Email:</Label>
+            <TextInput
+              type="email"
+              name="shopEmail"
+              value={formData.shopEmail}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Label htmlFor="shopWebsite">Shop Website:</Label>
+            <TextInput
+              type="url"
+              name="shopWebsite"
+              value={formData.shopWebsite}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Label htmlFor="shopOpeningHours">Shop Opening Hours:</Label>
+            <TextInput
+              type="text"
+              name="shopOpeningHours"
+              value={formData.shopOpeningHours}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Label htmlFor="isOpen">Is Open:</Label>
+            <input
+              type="checkbox"
+              name="isOpen"
+              checked={formData.isOpen}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* 
+          <div className="flex flex-col gap-4 flex-1">
+            <p className="font-semibold">
+              Imagex:{" "}
+              <span className="font-normal text-gray-600 ml-2">
+                2 Images Max
+              </span>
+            </p>
+
+            <div className="flex gap-4">
+              <FileInput
+                onChange={(e) => setFiles(e.target.files)}
+                type="file"
+                id="image"
+                accept="image/*"
+                multiple
+                className="w-full"
+              />
+              <button
+                onClick={handleImageSubmit}
+                type="button"
+                disabled={uploading}
+                className="p-1 text-red-700 border border-red-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+              >
+                {uploading ? "Uploading..." : "Upload"}
+              </button>
+            </div>
+            <p className="text-red-700">
+              {imageUploadError && imageUploadError}
+            </p>
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => (
+                <div
+                  key={`image-${index}`}
+                  className="flex justify-between p-3 border items-center"
+                >
+                  <img
+                    src={url}
+                    alt={`listing image ${index}`}
+                    className="w-20 h-20 object-contain rounded-lg"
+                  />
+                  <Button
+                    type="button"
+                    onClick={() => handleRemoveImage(index)}
+                    gradientDuoTone="pinkToOrange"
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ))}
+          </div>
+
+    */}
           <Button
             type="submit"
             gradientDuoTone="purpleToBlue"
@@ -130,9 +322,3 @@ const ShopCreate = () => {
 };
 
 export default ShopCreate;
-
-
-
-
-
-
